300d912fa2889b7d7befb21de1f325a58e27f45f
lesalaun <leo.salaun@etudiant.univ-rennes1.fr>
2023-01-06 20:58:31 +0100

    Ajout dessins

--- haikus.js
+++ haikus.js
@@ -1,28 +1,18 @@
 let haikus =
 [
     {
+        "1" : "Par-delà les montagnes embrumées",
+        "2" : "À l'instar d'une flèche perçante",
+        "3" : "L'oiseau se laisse tomber"
+    },
+    {
+        "1" : "Dès les prémisses du crépuscule",
+        "2" : "À l'instar des feuilles d'automne",
+        "3" : "L'éventail se déploie pleinement"
+    },
+    {
         "1" : "Au désert de teinte écarlate",
         "2" : "À l'instar de la fleur mourante",
         "3" : "S'effrite sa voix naïve"
-    },
-    {
-        "1" : "Dans la cité de miroirs",
-        "2" : "Lumière rencontre cristal au prix du vrai",
-        "3" : "Votre amour nous le haïssons"
-    },
-    {
-        "1" : "Prison du seigneur de lumière",
-        "2" : "Sueur rencontre flou au prix du sommeil",
-        "3" : "Pas un ne supportera davantage"
-    },
-    {
-        "1" : "Par delà les mers salées",
-        "2" : "À l'instar de la femme barbue",
-        "3" : "Sifflent tous les torrents enragés"
-    },
-    {
-        "1" : "Perdu dans le jardin ensoleillé",
-        "2" : "À l'instar des barreaux de plomb",
-        "3" : "Ils les regardent mourir gaiement"
     }
 ]
